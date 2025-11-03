61dcaa120f40bba52531810a54d9dade472cd5e4
Justin Jo <justinj7@gmail.com>
2025-11-03 17:54:58 -0500

    Added solutions for 2019 days 1-2

---
+++ 2019/advent_2019_day_1.py
@@ -0,0 +1,27 @@
+from advent_day import AdventDay
+
+class Advent2019Day1(AdventDay):
+
+    def _fuel_calc(self, mass: int) -> int:
+        return mass//3 - 2
+
+    def part_one(self) -> int:
+        self._convert_input_to_int()
+        return sum([self._fuel_calc(mass) for mass in self.input_int_array])
+
+    def part_two(self) -> int:
+        self._convert_input_to_int()
+        total_fuel = 0
+        mass_fuel = 0
+        incremental_fuel = 0
+        for mass in self.input_int_array:
+            incremental_fuel = self._fuel_calc(mass)
+            while incremental_fuel > 0:
+                mass_fuel += incremental_fuel
+                incremental_fuel = self._fuel_calc(incremental_fuel)
+            total_fuel += mass_fuel
+            incremental_fuel = mass_fuel = 0
+        return total_fuel
+
+
+Advent2019Day1().run()
---
+++ 2019/advent_2019_day_2.py
@@ -0,0 +1,44 @@
+from advent_day import AdventDay
+from copy import deepcopy
+
+class Advent2019Day2(AdventDay):
+
+    def _format_input(self, arr: list[int], noun: int = 12, verb: int = 2) -> None:
+        arr[1] = noun
+        arr[2] = verb
+
+    def _execute(self, arr: list[int]) -> None:
+        i = 0
+        while True:
+            opcode = arr[i]
+            if opcode == 99:
+                break
+            input_1 = arr[i+1]
+            input_2 = arr[i+2]
+            location = arr[i+3]
+            if opcode == 1:
+                arr[location] = arr[input_1] + arr[input_2]
+            elif opcode == 2:
+                arr[location] = arr[input_1] * arr[input_2]
+            i += 4
+
+    def part_one(self, arr: list[int] = []) -> int:
+        self._convert_input_to_int()
+        arr = deepcopy(self.input_int_array)
+        self._format_input(arr)
+        self._execute(arr)
+        return arr[0]
+
+    def part_two(self) -> int:
+        self._convert_input_to_int()
+        for n in range(0, 100):
+            for v in range(0, 100):
+                arr = deepcopy(self.input_int_array)
+                self._format_input(arr, n, v)
+                self._execute(arr)
+                if arr[0] == 19690720:
+                    return 100 * n + v
+        return -1
+
+
+Advent2019Day2().run()
