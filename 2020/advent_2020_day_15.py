4adf5d5aa3874990fb2202263ac968dd134f84d8
Justin Jo <justinj7@gmail.com>
2025-11-13 23:44:45 -0500

    Added optimization for 2020 day 15 part 2

--- 2020/advent_2020_day_15.py
+++ 2020/advent_2020_day_15.py
@@ -24,13 +24,37 @@ class Advent2020Day15(AdventDay):
 
         return curr_num
 
+    def _play_game_until_with_optimization(self, final_turn: int) -> int:
+        most_recent = defaultdict(int)
+        turn = 1
+        i = curr_num = prev_num = 0
+
+        while turn <= final_turn:
+            if i < len(self.input_int_array):
+                curr_num = self.input_int_array[i]
+                i += 1
+            else:
+                if prev_num in most_recent:
+                    curr_num = (turn - 1) - most_recent[prev_num]
+                else:
+                    curr_num = 0
+
+            most_recent[prev_num] = turn - 1
+            prev_num = curr_num
+            turn += 1
+
+        return curr_num
+
     def part_one(self) -> int:
         self._convert_input_to_int()
         return self._play_game_until(2020)
 
     def part_two(self) -> int:
         # takes a bit under 15 seconds to run
-        return self._play_game_until(30000000)
+        # return self._play_game_until(30000000)
+
+        # takes ~7 seconds to run
+        return self._play_game_until_with_optimization(30000000)
 
 
 Advent2020Day15().run()
