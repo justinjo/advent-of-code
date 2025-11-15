aff02d3555bbcdaf87f8c9cfcf657292f04ac0b7
Justin Jo <justinj7@gmail.com>
2025-11-15 18:39:32 -0500

    Added solution for 2021 day 2 part 2

--- 2021/advent_2021_day_02.py
+++ 2021/advent_2021_day_02.py
@@ -5,15 +5,32 @@ class Submarine:
     def __init__(self) -> None:
         self.horiz = 0
         self.depth = 0
+        self.aim = 0
 
-    def move(self, instruction: str) -> None:
-        direction, value = instruction.split(' ')
+    def _move_without_aim(self, direction: str, value: int) -> None:
         if direction == 'forward':
-            self.horiz += int(value)
+            self.horiz += value
         elif direction == 'down':
-            self.depth += int(value)
+            self.depth += value
         elif direction == 'up':
-            self.depth -= int(value)
+            self.depth -= value
+
+    def _move_with_aim(self, direction: str, value: int) -> None:
+        if direction == 'forward':
+            self.horiz += value
+            self.depth += self.aim * value
+        elif direction == 'down':
+            self.aim += value
+        elif direction == 'up':
+            self.aim -= value
+
+
+    def move(self, instruction: str, should_aim: bool = False) -> None:
+        direction, value = instruction.split(' ')
+        if should_aim:
+            self._move_with_aim(direction, int(value))
+        else:
+            self._move_without_aim(direction, int(value))
 
 
 class Advent2021Day02(AdventDay):
@@ -24,9 +41,11 @@ class Advent2021Day02(AdventDay):
             sub.move(instr)
         return sub.horiz * sub.depth
 
-
     def part_two(self) -> int:
-        ...
+        sub = Submarine()
+        for instr in self.input_str_array:
+            sub.move(instr, should_aim=True)
+        return sub.horiz * sub.depth
 
 
 Advent2021Day02().run()
