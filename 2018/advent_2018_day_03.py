f170c4c495db5d0f5644032322a39918137736fd
Justin Jo <justinj7@gmail.com>
2025-11-14 19:29:25 -0500

    Added solution for 2018 day 3 part 2

--- 2018/advent_2018_day_03.py
+++ 2018/advent_2018_day_03.py
@@ -6,13 +6,13 @@ class Advent2018Day03(AdventDay):
     def _parse_input(self) -> None:
         self.claims = []
         for s in self.input_str_array:
-            claim_number, claim = s[1:].split(' @ ')
+            claim_id, claim = s.split(' @ ')
             distances, dimensions = claim.split(': ')
             col_distance, row_distance = [int(d) for d in distances.split(',')]
             cols, rows = [int(d) for d in dimensions.split('x')]
             self.claims.append(
                 {
-                    'id': claim_number,
+                    'id': int(claim_id[1:]),
                     'col_distance': col_distance,
                     'row_distance': row_distance,
                     'cols': cols,
@@ -20,10 +20,8 @@ class Advent2018Day03(AdventDay):
                 }
             )
 
-    def part_one(self) -> int:
-        self._parse_input()
+    def _get_coord_claims_map(self) -> dict[tuple[int, int], list[int]]:
         coord_claims_map = defaultdict(list)
-        multiclaimed_squares = 0
 
         for claim in self.claims:
             for r in range(
@@ -35,6 +33,12 @@ class Advent2018Day03(AdventDay):
                     claim['cols'] + claim['col_distance']
                 ):
                     coord_claims_map[(r, c)].append(claim['id'])
+        return coord_claims_map
+
+    def part_one(self) -> int:
+        self._parse_input()
+        multiclaimed_squares = 0
+        coord_claims_map = self._get_coord_claims_map()
 
         for coord in coord_claims_map:
             if len(coord_claims_map[coord]) >= 2:
@@ -43,7 +47,14 @@ class Advent2018Day03(AdventDay):
         return multiclaimed_squares
 
     def part_two(self) -> int:
-        ...
+        # self._parse_input
+        coord_claims_map = self._get_coord_claims_map()
+        claim_set = set([claim['id'] for claim in self.claims])
+        for coord in coord_claims_map:
+            claims = coord_claims_map[coord]
+            if len(claims) > 1:
+                claim_set -= set(claims)
+        return list(claim_set)[0]
 
 
 Advent2018Day03().run()
