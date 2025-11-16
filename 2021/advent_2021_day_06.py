c57827f8a1d338d780a5b060610e510e496c1d42
Justin Jo <justinj7@gmail.com>
2025-11-15 22:14:05 -0800

    Added solution for 2021 day 6 part 2

--- 2021/advent_2021_day_06.py
+++ 2021/advent_2021_day_06.py
@@ -7,18 +7,22 @@ class Advent2021Day06(AdventDay):
         c = Counter([int(n) for n in self.input_str_array[0].split(',')])
         return c
 
+    def spawn(self, fish: Counter, days: int) -> int:
+        for _ in range(days):
+            spawned = fish[0]
+            for i in range(8):
+                fish[i] = fish[i+1]
+            fish[6] += spawned
+            fish[8] = spawned
+        return sum(fish.values())
+
     def part_one(self) -> int:
         fish_counter = self._parse_input()
-        for _ in range(80):
-            spawned = fish_counter[0]
-            for i in range(8):
-                fish_counter[i] = fish_counter[i+1]
-            fish_counter[6] += spawned
-            fish_counter[8] = spawned
-        return sum(fish_counter.values())
+        return self.spawn(fish_counter, 80)
 
     def part_two(self) -> int:
-        ...
+        fish_counter = self._parse_input()
+        return self.spawn(fish_counter, 256)
 
 
 Advent2021Day06().run()
