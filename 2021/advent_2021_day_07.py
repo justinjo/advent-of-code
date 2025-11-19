da091bd464a2396debc7b3a3c7a877631045f0ee
Justin Jo <justinj7@gmail.com>
2025-11-19 14:59:46 -0800

    Added solution for 2021 day 7 part 2

--- 2021/advent_2021_day_07.py
+++ 2021/advent_2021_day_07.py
@@ -1,6 +1,24 @@
 from advent_day import AdventDay
+from collections import Counter
 
 class Advent2021Day07(AdventDay):
+
+    sigma_arr = [0]
+
+    def get_sigma(self, val: int) -> int:
+        if val >= len(self.sigma_arr):
+            for i in range(len(self.sigma_arr), val + 1):
+                self.sigma_arr.append(self.sigma_arr[-1] + i)
+        return self.sigma_arr[val]
+
+    def calculate_fuel(self, fewer: Counter, greater: Counter) -> int:
+        fuel = 0
+        for pos in fewer:
+            fuel += self.get_sigma(pos) * fewer[pos]
+        for pos in greater:
+            fuel += self.get_sigma(pos) * greater[pos]
+        return fuel
+
 
     def part_one(self) -> int:
         self._convert_input_to_int()
@@ -21,7 +39,20 @@ class Advent2021Day07(AdventDay):
         return min_fuel
 
     def part_two(self) -> int:
-        ...
+        # self._convert_input_to_int()
+        max_val = max(sorted(self.input_int_array))
+        fewer = Counter() # num crabs n positions less than pos
+        greater = Counter(self.input_int_array) # num crabs n positions greater than pos
+        min_fuel = float('inf')
+        # pos is position checked against all crabs
+        for _ in range(max_val + 1):
+            fewer[0] = greater[0]
+            for pos in range(max(sorted(greater)) + 1):
+                greater[pos] = greater[pos+1]
+            for pos in reversed(range(max(sorted(fewer)) + 2)):
+                fewer[pos] = fewer[pos-1]
+            min_fuel = min(self.calculate_fuel(fewer, greater), min_fuel)
+        return int(min_fuel)
 
 
 Advent2021Day07().run()
