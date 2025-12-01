ed593d22ee934beb2e901a61e51391bf328d24c5
Justin Jo <justinj7@gmail.com>
2025-12-01 02:21:05 -0500

    Added solution for 2025 day 1 part 2

--- 2025/advent_2025_day_01.py
+++ 2025/advent_2025_day_01.py
@@ -10,9 +10,19 @@ class Advent2025Day01(AdventDay):
             password += 1 if dial % 100 == 0 else 0
         return password
 
-
     def part_two(self) -> int:
-        ...
+        password = 0
+        dial = 50
+        for rotation in self.input_str_array:
+            delta = int(rotation[1:]) if rotation[0] == 'R' else -int(rotation[1:])
+            if rotation[0] == 'R':
+                password += (dial + delta) // 100
+            else:
+                password += -((dial + delta) // 100)
+                password -= 1 if dial == 0 else 0
+                password += 1 if (dial + delta) % 100 == 0 else 0
+            dial = (dial + delta) % 100
+        return password
 
 
 Advent2025Day01().run()
