3d6c7332f50c749c3941c91e98446ea2e731dabf
Justin Jo <justinj7@gmail.com>
2025-12-04 08:54:52 -0500

    Added solution for 2025 day 4 part 2

--- 2025/advent_2025_day_04.py
+++ 2025/advent_2025_day_04.py
@@ -2,8 +2,8 @@ from advent_day import AdventDay
 
 class Advent2025Day04(AdventDay):
 
-    def accessible_rolls(self, grid: list[str]) -> int:
-        ar = 0
+    def list_accessible_rolls(self, grid: list[str]) -> list[tuple[int, int]]:
+        ar = []
         for y in range(len(grid)):
             for x in range(len(grid[0])):
                 if grid[y][x] != '@':
@@ -15,13 +15,35 @@ class Advent2025Day04(AdventDay):
                             continue
                         if 0 <= x+x_d < len(grid[0]) and 0 <= y+y_d < len(grid):
                             neighbors += 1 if grid[y+y_d][x+x_d] == '@' else 0
-                ar += 1 if neighbors < 4 else 0
+                if neighbors < 4:
+                    ar.append((x, y))
         return ar
 
+    def remove_rolls(self, grid: list[str], rolls: list[tuple[int, int]]):
+        new_grid = []
+        roll_set = set(rolls)
+        for y in range(len(grid)):
+            new_line = ''
+            for x in range(len(grid[0])):
+                new_line += 'x' if (x, y) in roll_set else grid[y][x]
+            new_grid.append(new_line)
+        return new_grid
+
+    def count_accessible_rolls(self, grid: list[str]) -> int:
+        return len(self.list_accessible_rolls(grid))
+
     def part_one(self) -> int:
-        return self.accessible_rolls(self.input_str_array)
+        return self.count_accessible_rolls(self.input_str_array)
 
     def part_two(self) -> int:
-        ...
+        removed = 0
+        grid = self.input_str_array
+        accessible_rolls = self.list_accessible_rolls(grid)
+        while accessible_rolls:
+            grid = self.remove_rolls(grid, accessible_rolls)
+            removed += len(accessible_rolls)
+            accessible_rolls = self.list_accessible_rolls(grid)
+        return removed
+
 
 Advent2025Day04().run()
