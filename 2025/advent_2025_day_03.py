2c9f00359649836dd4395f1169b395037f682829
Justin Jo <justinj7@gmail.com>
2025-12-03 20:07:14 -0500

    Added solution for 2025 day 3 part 2

--- 2025/advent_2025_day_03.py
+++ 2025/advent_2025_day_03.py
@@ -2,17 +2,23 @@ from advent_day import AdventDay
 
 class Advent2025Day03(AdventDay):
 
-    def part_one(self) -> int:
-        joltage = 0
+    def sum_joltage(self, digits: int = 2) -> int:
+        joltage = 0 # should be doing monotonic stacks but lazy
         for s in self.input_str_array:
+            i = j = 0
             arr = [int(x) for x in list(s)]
-            b1 = max(arr[:-1])
-            i1 = arr.index(b1)
-            b2 = max(arr[i1+1:])
-            joltage += b1 * 10 + b2
+            for t in range(digits-1,-1,-1):
+                subarr = arr[i:len(arr)-t] # use len to prevent arr[x:0]
+                b = max(subarr)
+                i += subarr.index(b) + 1
+                j = j * 10 + b
+            joltage += j
         return joltage
 
+    def part_one(self) -> int:
+        return self.sum_joltage()
+
     def part_two(self) -> int:
-        ...
+        return self.sum_joltage(12)
 
 Advent2025Day03().run()
