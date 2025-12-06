92214c980e44a589053a1c9580cb15c61d00d8b4
Justin Jo <justinj7@gmail.com>
2025-12-06 17:57:57 -0500

    Added python file template

---
+++ python/advent_template.py
@@ -0,0 +1,11 @@
+def part_one(input_arr: list[str]) -> int: ...
+
+
+def part_two(input_arr: list[str]) -> int: ...
+
+
+input_arr: list[str] = open("advent_YYYY_day_DD.txt").read().splitlines()
+
+print("Advent of Code YYYY - Day DD")
+print(f"Part One: {part_one(input_arr)}")
+print(f"Part Two: {part_two(input_arr)}")
