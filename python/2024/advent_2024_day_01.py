a06439edfb7e5a131161ea4cba1882a77d62f497
Justin Jo <justinj7@gmail.com>
2025-12-05 23:31:17 -0500

    Removed AdventDay from 2024 solutions

--- python/2024/advent_2024_day_01.py
+++ python/2024/advent_2024_day_01.py
@@ -1,31 +1,31 @@
-from advent_day import AdventDay
 from collections import Counter
 
-class Advent2024Day01(AdventDay):
+def parse_input(input_arr: list[str]) -> tuple[list[int], list[int]]:
+    left = []
+    right = []
+    for l in input_arr:
+        l, r = l.split('   ')
+        left.append(int(l))
+        right.append(int(r))
+    return (left, right)
 
-    def parse_input(self) -> tuple[list[int], list[int]]:
-        left = []
-        right = []
-        for l in self.input_str_array:
-            l, r = l.split('   ')
-            left.append(int(l))
-            right.append(int(r))
-        return (left, right)
+def part_one(input_arr: list[str]) -> int:
+    left, right = parse_input(input_arr)
+    distance = 0
+    for l, r in zip(sorted(left), sorted(right)):
+        distance += abs(r - l)
+    return distance
 
-    def part_one(self) -> int:
-        left, right = self.parse_input()
-        distance = 0
-        for l, r in zip(sorted(left), sorted(right)):
-            distance += abs(r - l)
-        return distance
+def part_two(input_arr: list[str]) -> int:
+    left, right = parse_input(input_arr)
+    c = Counter(right)
+    similarity = 0
+    for l in left:
+        similarity += l * c[l]
+    return similarity
 
-    def part_two(self) -> int:
-        left, right = self.parse_input()
-        c = Counter(right)
-        similarity = 0
-        for l in left:
-            similarity += l * c[l]
-        return similarity
+input_arr: list[str] = open('advent_2024_day_01.txt').read().splitlines()
 
-
-Advent2024Day01().run()
+print('Advent of Code 2024 - Day 01')
+print(f'Part One: {part_one(input_arr)}')
+print(f'Part Two: {part_two(input_arr)}')
