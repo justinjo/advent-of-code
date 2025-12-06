92cc4ddbe74e34ee5d42313c771c7fec65ccbaea
Justin Jo <justinj7@gmail.com>
2025-12-06 16:56:29 -0500

    Refactored solution to 2025 day 6

--- python/2025/advent_2025_day_06.py
+++ python/2025/advent_2025_day_06.py
@@ -1,64 +1,42 @@
+def array_product(arr: list[str]) -> int:
+    return eval('*'.join(map(str, arr)))
+
 def part_one(input_arr: list[str]) -> int:
-    arr = []
-    total_sum = 0
-    for l in input_arr:
-        row = []
-        for op in l.split(' '):
-            if op:
-                row.append(op)
-        arr.append(row)
-    for i in range(len(arr[0])):
-        n = 0
-        if arr[len(arr)-1][i] == '+':
-            n = 0
-            for j in range(len(arr)-1):
-                n += int(arr[j][i])
-        else:
-            n = 1
-            for j in range(len(arr)-1):
-                n *= int(arr[j][i])
-        total_sum += n
-    return total_sum
+    arr = [[x for x in l.split(' ') if x] for l in input_arr] # trim whitespace
+    total = 0
+    for col in range(len(arr[0])):
+        operands = [arr[row][col] for row in range(len(arr)-1)]
+        if arr[-1][col] == '+':
+            total += sum([int(op) for op in operands])
+        elif arr[-1][col] == '*':
+            total += array_product(operands)
+    return total
 
 def part_two(input_arr: list[str]) -> int:
-    operator_i = operand_i = total = 0
-    is_calculating = True
-    while is_calculating:
+    operator_i = operand_i = len(input_arr[0]) - 1 # read right to left
+    total = 0
+    while operator_i >=0 and operand_i >= 0:
         # get operator
-        while input_arr[-1][operator_i] == ' ':
-            operator_i += 1
+        while operator_i >= 0 and input_arr[-1][operator_i] == ' ':
+            operator_i -= 1
         operator = input_arr[-1][operator_i]
-        operator_i += 1
+        operator_i -= 1
 
-        # get operands
+        # get operands, looping until the operator column
         operands = []
-        digit_found = True
-        while digit_found:
+        while operand_i > operator_i:
             operand = 0
-            digit_found = False
-            for i in range(len(input_arr) - 1):
-                if operand_i >= len(input_arr[0]):
-                    continue
-                digit = input_arr[i][operand_i]
-                if digit == ' ':
-                    continue
-                digit_found = True
-                operand = int(input_arr[i][operand_i]) + operand * 10
-            if digit_found:
-                operands.append(operand)
-            operand_i += 1
+            for row in range(len(input_arr) - 1):
+                if input_arr[row][operand_i] != ' ':
+                    operand = int(input_arr[row][operand_i]) + operand * 10
+            operands.append(operand)
+            operand_i -= 1
+        operand_i -= 1 # skip column of all whitespace
 
-        # calculate
         if operator == '+':
             total += sum(operands)
         elif operator == '*':
-            product = 1
-            for x in operands:
-                product *= x
-            total += product
-
-        # calc until end reached
-        is_calculating = operator_i < len(input_arr[0]) and operand_i < len(input_arr[-1])
+            total += array_product(operands)
     return total
 
 input_arr: list[str] = open('advent_2025_day_06.txt').read().splitlines()
